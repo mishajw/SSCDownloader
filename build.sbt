fc6b064f105cb1ee77d7cf90404f5b1fc1a208d5
Misha Wagner <mishajw@gmail.com>
2015-12-03 12:10:09 +0000

    Integrated Jsoup

--- build.sbt
+++ build.sbt
@@ -13,10 +13,11 @@ libraryDependencies ++= {
     "org.eclipse.jetty" % "jetty-webapp" % "8.1.7.v20120910"  % "container,test",
     "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" %
       "container,compile" artifacts Artifact("javax.servlet", "jar", "jar"),
-    "net.sourceforge.htmlcleaner" % "htmlcleaner" % "2.15",
+//    "net.sourceforge.htmlcleaner" % "htmlcleaner" % "2.15",
     "org.slf4j" % "slf4j-api" % "1.7.5",
     "org.slf4j" % "slf4j-simple" % "1.7.5",
     "org.clapper" %% "grizzled-slf4j" % "1.0.2",
-    "com.typesafe.akka" %% "akka-actor" % "2.3.11"
+    "com.typesafe.akka" %% "akka-actor" % "2.3.11",
+    "org.jsoup" % "jsoup" % "1.8.3"
   )
 }
--- src/main/scala/com/mishawagner/sscdownloader/backend/LinkCrawler.scala
+++ src/main/scala/com/mishawagner/sscdownloader/backend/LinkCrawler.scala
@@ -1,12 +1,8 @@
 package com.mishawagner.sscdownloader.backend
 
-import java.io.IOException
-import java.net.MalformedURLException
-
 import grizzled.slf4j.Logging
-import org.htmlcleaner.HtmlCleaner
-
-import scala.io.Source
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
 /**
  * Created by misha on 12/11/15.
@@ -14,6 +10,9 @@ import scala.io.Source
  * Helps retrieve links from a webpage
  */
 object LinkCrawler extends Logging {
+
+  private val LINK_ATTR = Array("href", "src")
+
 
   /**
    * Get all the links from a page, filtered by suffix
@@ -31,80 +30,17 @@ object LinkCrawler extends Logging {
     }
   }
 
-  /**
-   * Get all the links on a page
-   * @param url page url
-   * @return list of links
-   */
-  def getLinksForPage(url: String): Seq[String] =
-    this getLinksForHTML (url, this getHTMLForPage url)
-
-  /**
-   * Get the html for a page
-   * @param url url to fetch HTML from
-   * @return
-   */
-  private def getHTMLForPage(url: String): String =
+  def getLinksForPage(url: String): List[String] = {
     try {
-      info("Getting HTML for a page to be crawled")
-      (Source fromURL url).mkString
+      val doc = Jsoup.connect(url).get()
+
+      LINK_ATTR
+        .flatMap(doc.getElementsByAttribute(_).toArray)
+        .map(_.asInstanceOf[Element])
+        .map(e => e.attr("abs:" + LINK_ATTR.filter(e.hasAttr).head))
+        .toList
     } catch {
-      case e: MalformedURLException =>
-        println("Not a valid URL.") ; ""
-      case e: IOException =>
-        println("Couldn't reach URL.") ; ""
-      case e: Exception =>
-        println("Unknown error") ; e.printStackTrace() ; ""
-    }
-
-  /**
-   * Get links from a HTML string
-   * @param baseUrl The original url of the page
-   * @param html the html
-   * @return list of links
-   */
-  private def getLinksForHTML(baseUrl: String, html: String): Seq[String] = {
-    info("Getting links out of a page")
-
-    val cleaner = new HtmlCleaner
-    val root = cleaner clean html
-    val results =
-        (root getElementsHavingAttribute ("href", true)) ++
-        (root getElementsHavingAttribute ("src", true))
-    
-    results
-      .map(node => node getAttributeByName { if (node.hasAttribute("href")) "href" else "src" })
-      .map(url => formatUrl(url, baseUrl))
-  }
-
-  /**
-   * Format a url so it contains protocol, domain, and path
-   * @param url the url to format
-   * @param baseUrl the site it came from
-   * @return the formatted url
-   */
-  private def formatUrl(url: String, baseUrl: String): String = {
-    if (url.split("://").length > 1) {
-      // If it's already got a protocol, it's valid
-      url
-    } else if (url.startsWith("//")) {
-      // If it's got a // but not protocol
-      "http:" + url
-    } else if ({
-      val levelSplit = url.split("/").toSeq
-      levelSplit.nonEmpty && levelSplit.head.split(".").length > 1
-    }) {
-      // If it's got a domain but no protocol
-      "http://" + url
-    } else if (baseUrl.endsWith("/") && url.startsWith("/")) {
-      // If it's just got the location, but with a slash on both sides
-      baseUrl + url.tail
-    } else if (baseUrl.endsWith("/") || url.startsWith("/")) {
-      // If it's only got a slash on one side
-      baseUrl + url
-    } else {
-      // If it's got no slashes
-      baseUrl + "/" + url
+      case e: Throwable => List()
     }
   }
 }
